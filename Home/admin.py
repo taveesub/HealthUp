d4e0d0c70e6ad29997854c5e9c1e48c3715c4882
rachata <rachata_2542_@hotmail.com>
2021-06-02 20:51:02 +0700

    admin

--- Home/admin.py
+++ Home/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Person
+from .models import Sport
+from .models import Health
+
+admin.site.register(Person)
+admin.site.register(Sport)
+admin.site.register(Health)
